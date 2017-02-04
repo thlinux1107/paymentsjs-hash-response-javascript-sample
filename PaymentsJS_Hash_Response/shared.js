bd87bcb5cbad10a4aa0a1e37642eb98c29e4b1c5
Thomas Hagan <thlinux1107@users.noreply.github.com>
2017-02-04 17:12:28 -0500

    Updated comments.

--- PaymentsJS_Hash_Response/shared.js
+++ PaymentsJS_Hash_Response/shared.js
@@ -1,3 +1,5 @@
+//TH - Shared variables that should not be displayed on the main page.
+//TH - The 'clientSecret' corresponds with the sample 'ccresponsestring' on PaymentsJS_response_parse.html page.
 var shared = {
 	clientSecret	:	"XVSc6Ouo0YObDLHD"
 }
